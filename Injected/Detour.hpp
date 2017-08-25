04a660329aa52ee5e59d3387e6f20579930c960a
ARNFRIED <q.bufwbkb@gmail.com>
2017-08-24 23:57:07 -0400

    a

--- Injected/Detour.hpp
+++ Injected/Detour.hpp
@@ -8,16 +8,16 @@ public:
 		: target{ (byte*)target_func }
 		, hook{ hook_func }
 	{
-		DWORD old_protection_;
-		VirtualProtect(target, 6, PAGE_EXECUTE_READWRITE, &old_protection_);
-
 		new_bytes.push_back(0x68);				// push (the address provided through hook)
 		new_bytes.resize(5);
 		*(int*)(new_bytes.data() + 1) = hook;	// dirty hack
 		new_bytes.push_back(0xc3);				// return
 
 		original_bytes.resize(6);
+		DWORD dummy;
+		VirtualProtect(target, 6, PAGE_EXECUTE_READWRITE, &old_protection);
 		memcpy(original_bytes.data(), target, 6);
+		VirtualProtect(target, 6, old_protection, &dummy);
 
 		Apply();
 	}
@@ -29,12 +29,18 @@ public:
 
 	void Apply()
 	{
+		DWORD dummy;
+		VirtualProtect(target, 6, PAGE_EXECUTE_READWRITE, &old_protection);
 		memcpy(target, new_bytes.data(), 6);
+		VirtualProtect(target, 6, old_protection, &dummy);
 	}
 
 	void Restore()
 	{
+		DWORD dummy;
+		VirtualProtect(target, 6, PAGE_EXECUTE_READWRITE, &old_protection);
 		memcpy(target, original_bytes.data(), 6);
+		VirtualProtect(target, 6, old_protection, &dummy);
 	}
 
 	template<typename T, typename... Args >
@@ -50,5 +56,6 @@ public:
 	std::vector<byte> new_bytes{};
 	byte* target{};
 private:
-	int hook{};	
+	int hook{};
+	DWORD old_protection;
 };
